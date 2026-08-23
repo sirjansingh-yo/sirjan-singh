bd444f664cfe8907c14f3a816fafadb9521226f6
sirjansingh-yo <Sirjan.38352@stu.upes.ac.in>
2026-08-23 16:17:36 +0530

    Add files via upload

---
+++ D1(Q2).c
@@ -0,0 +1,23 @@
+//Q2:Write a program to input two numbers and display their sum, difference, product, and quotient.
+Input 1:
+10 2
+Output 1:
+Sum=12, Diff=8, Product=20, Quotient=5
+Input 2:
+7 3
+Output 2:
+Sum=10, Diff=4, Product=21, Quotient=2
+
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    printf("Sum=%d, Diff=%d, Product=%d", a+b, a-b, a*b);
+    if(b != 0)
+        printf(", Quotient=%d", a/b);
+    else
+        printf(", Quotient=Undefined");
+    return 0;
+}
